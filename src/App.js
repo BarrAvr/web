35421d66082b1db96cb7aa784dfc4702eb0f62b8
Barr <barr@medisensor.tech>
2025-06-23 18:34:04 -0700

    Testing adding images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import myProfilePhoto from './assets/myProfilePhoto.jpg';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={myProfilePhoto} alt="My Profile Photo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
